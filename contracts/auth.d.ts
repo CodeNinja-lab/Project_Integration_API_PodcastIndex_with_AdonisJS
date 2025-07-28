d0f5d4fcba4b785dccb7efa4e3de94eefcf722b4
CodeNinja <14mbaye28@gmail.com>
2025-07-28 11:05:32 +0000

    suppression du dossier contract

--- contracts/auth.d.ts
+++
@@ -1,8 +0,0 @@
-import type { InferAccessTokensProvider } from '@adonisjs/auth/access_tokens'
-import type User from '#models/user'
-
-declare module '@adonisjs/auth/access_tokens' {
-  interface UserWithTokens {
-    accessTokens: InferAccessTokensProvider<typeof User>
-  }
-}
